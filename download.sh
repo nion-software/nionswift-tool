a1ce638a976449f92f13dc612a597b274fc6b240
Chris Meyer <cmeyer1969@gmail.com>
2019-01-08 09:44:38 -0800

    Remove unused files.

--- download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# note: pass the version; but also change it in each of the setup scripts.
-
-version=$1
-
-curl -L -O https://github.com/nion-software/nionswift-launcher/releases/download/$version/NionSwiftLauncher-Mac.zip
-curl -L -O https://github.com/nion-software/nionswift-launcher/releases/download/$version/NionSwiftLauncher-Linux.zip
-curl -L -O https://github.com/nion-software/nionswift-launcher/releases/download/$version/NionSwiftLauncher-Windows.zip
